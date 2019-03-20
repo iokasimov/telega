e012230944fddd8d0f7340b0381d0dd5c65078ce
Murat Kasimov <iokasimov.m@gmail.com>
2019-03-20 18:23:51 +0300

    Define Token and Telegram datatypes

--- Network/Telegram/API/Bot.hs
+++ Network/Telegram/API/Bot.hs
@@ -1,5 +1,14 @@
-module Network.Telegram.API.Bot (module Exports) where
+module Network.Telegram.API.Bot (module Exports, Telegram, Token) where
+
+import "servant" Servant.API (FromHttpApiData, ToHttpApiData)
+import "servant-client" Servant.Client (ClientM)
+import "text" Data.Text (Text)
+import "transformers" Control.Monad.Trans.Reader (ReaderT)
 
 import Network.Telegram.API.Bot.Chat as Exports
 import Network.Telegram.API.Bot.From as Exports
 import Network.Telegram.API.Bot.Message as Exports
+
+newtype Token = Token Text deriving (Eq, FromHttpApiData, ToHttpApiData)
+
+type Telegram e a = ReaderT (Token, e) ClientM a
