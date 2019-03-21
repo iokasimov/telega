73e0516ed5e66fd1290b49a9e905a1e066c52d6d
Murat Kasimov <iokasimov.m@gmail.com>
2019-03-21 15:27:45 +0300

    Define Telegram runner function

--- Network/Telegram/API/Bot.hs
+++ Network/Telegram/API/Bot.hs
@@ -1,9 +1,11 @@
-module Network.Telegram.API.Bot (module Exports, Telegram, Token) where
+module Network.Telegram.API.Bot (module Exports, Telegram, ask', Token (Token)) where
 
-import "servant" Servant.API (FromHttpApiData, ToHttpApiData)
-import "servant-client" Servant.Client (ClientM)
-import "text" Data.Text (Text)
-import "transformers" Control.Monad.Trans.Reader (ReaderT)
+import "http-client" Network.HTTP.Client (Manager)
+import "servant" Servant.API (Capture, FromHttpApiData, ToHttpApiData (toUrlPiece))
+import "servant-client" Servant.Client (BaseUrl (BaseUrl), ClientEnv, ClientM
+	, ServantError, Scheme (Https), mkClientEnv, runClientM)
+import "text" Data.Text (Text, unpack)
+import "transformers" Control.Monad.Trans.Reader (ReaderT (runReaderT), ask)
 
 import Network.Telegram.API.Bot.Chat as Exports
 import Network.Telegram.API.Bot.From as Exports
@@ -12,3 +14,12 @@ import Network.Telegram.API.Bot.Message as Exports
 newtype Token = Token Text deriving (Eq, FromHttpApiData, ToHttpApiData)
 
 type Telegram e a = ReaderT (Token, e) ClientM a
+
+telegram :: Manager -> Token -> e -> Telegram e a -> IO (Either ServantError a)
+telegram manager token env = flip runClientM (mkClientEnv manager $ base_url token) . flip runReaderT (token, env) where
+
+	base_url :: Token -> BaseUrl
+	base_url = BaseUrl Https "api.telegram.org" 443 . unpack . toUrlPiece
+
+ask' :: Telegram e e
+ask' = snd <$> ask
