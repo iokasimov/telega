e5d35fb136a493f8c8969f96cab04666110b4dd4
Murat Kasimov <iokasimov.m@gmail.com>
2019-03-24 23:57:51 +0300

    Sketch up runner, fix imports

--- Network/Telegram/API/Bot/Capacity/Postable.hs
+++ Network/Telegram/API/Bot/Capacity/Postable.hs
@@ -1,10 +1,10 @@
 module Network.Telegram.API.Bot.Capacity.Postable (Postable (..)) where
 
 import "aeson" Data.Aeson (FromJSON, Value, decode)
-import "base" Control.Exception (SomeException, try)
+import "base" Control.Exception (try)
 import "base" Data.Maybe (fromJust)
 import "http-client" Network.HTTP.Client (Response (responseBody))
-import "text" Data.Text (Text, unpack)
+import "text" Data.Text (unpack)
 import "transformers" Control.Monad.Trans.Class (lift)
 import "transformers" Control.Monad.Trans.Except (ExceptT (ExceptT))
 import "transformers" Control.Monad.Trans.Reader (ask)
--- Network/Telegram/API/Bot/Core.hs
+++ Network/Telegram/API/Bot/Core.hs
@@ -1,4 +1,4 @@
-module Network.Telegram.API.Bot.Core (Telegram, ask', Token (Token)) where
+module Network.Telegram.API.Bot.Core (Telegram, ask', Token (..), telegram) where
 
 import "base" Control.Exception (SomeException)
 import "http-client" Network.HTTP.Client (Manager)
@@ -7,11 +7,14 @@ import "servant-client" Servant.Client (BaseUrl (BaseUrl), ClientEnv, ClientM
 	, ServantError, Scheme (Https), mkClientEnv, runClientM)
 import "text" Data.Text (Text, unpack)
 import "transformers" Control.Monad.Trans.Reader (ReaderT (runReaderT), ask)
-import "transformers" Control.Monad.Trans.Except (ExceptT)
+import "transformers" Control.Monad.Trans.Except (ExceptT, runExceptT)
 
 newtype Token = Token Text deriving Eq
 
 type Telegram e a = ReaderT (Token, e) (ExceptT SomeException IO) a
+
+telegram :: Manager -> Token -> e -> Telegram e a -> IO (Either SomeException a)
+telegram manager token env = runExceptT . flip runReaderT (token, env)
 
 ask' :: Telegram e e
 ask' = snd <$> ask
