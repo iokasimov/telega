a7468a5082e8042fec5bb52326fd03d82e52ea24
Murat Kasimov <iokasimov.m@gmail.com>
2019-03-28 21:39:27 +0300

    Try to parse bot commands into separated constructor of Message

--- Network/Telegram/API/Bot/Object/Message.hs
+++ Network/Telegram/API/Bot/Object/Message.hs
@@ -1,9 +1,13 @@
 module Network.Telegram.API.Bot.Object.Message
 	(Message (..), Initial, Marking) where
 
-import "aeson" Data.Aeson (FromJSON (parseJSON), object, withObject, (.:), (.=))
+import "aeson" Data.Aeson (FromJSON (parseJSON), object, withArray, withObject, (.:), (.=))
+import "aeson" Data.Aeson.Types (Array, Object, Parser, Value)
+import "base" Control.Applicative (Alternative ((<|>), empty))
 import "base" Data.Int (Int64)
 import "text" Data.Text (Text)
+
+import qualified "text" Data.Text as T (drop, take)
 
 import Network.Telegram.API.Bot.Capacity.Postable
 	(Initial, Postable (initial_value, post_endpoint))
@@ -13,15 +17,36 @@ import Network.Telegram.API.Bot.Object.Chat (Chat)
 import Network.Telegram.API.Bot.Object.From (From)
 import Network.Telegram.API.Bot.Object.Keyboard (Keyboard)
 
-data Message = Message Int Chat From Text deriving Show
+data Message
+	= Textual Int Chat From Text
+	| Command Int Chat From Text
+	deriving Show
 
 type instance Initial Message = (Int64, Text, Maybe Keyboard)
 type instance Marking Message = (Int64, Int)
 
 instance FromJSON Message where
-	parseJSON = withObject "Message" $ \v ->
-		Message <$> v .: "message_id" <*> v .: "chat"
-			<*> v .: "from" <*> v .: "text"
+	parseJSON = withObject "Message" $ \v -> command v <|> textual v where
+
+		command :: Object -> Parser Message
+		command v = Command <$> v .: "message_id" <*> v .: "chat" <*> v .: "from"
+			<*> (v .: "entities" >>= command_entity >>= extract_command v)
+
+		textual :: Object -> Parser Message
+		textual v = Textual <$> v .: "message_id"
+			<*> v .: "chat" <*> v .: "from" <*> v .: "text"
+
+		command_entity :: Value -> Parser (Int, Int)
+		command_entity = withArray "Command entity" $ \a ->
+			foldr ((<|>) . entity) empty a where
+
+			entity :: Value -> Parser (Int, Int)
+			entity = withObject "Command entity" $ \v -> v .: "type" >>= \case
+				("bot_command" :: Text) -> (,) <$> v .: "offset" <*> v .: "length"
+				_ -> fail "It's not a bot command"
+
+		extract_command :: Object -> (Int, Int) -> Parser Text
+		extract_command v (ofs, len) = (T.take len . T.drop ofs) <$> v .: "text"
 
 instance Postable Message where
 	initial_value (chat_id, text, Nothing) =
