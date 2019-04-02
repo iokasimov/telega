b53bef973b13606f01fc44a893226b00fd839229
Murat Kasimov <iokasimov.m@gmail.com>
2019-04-02 17:34:18 +0300

    Change type of identificator of Callback datatype

--- Network/Telegram/API/Bot/Object/Callback.hs
+++ Network/Telegram/API/Bot/Object/Callback.hs
@@ -1,12 +1,11 @@
 module Network.Telegram.API.Bot.Object.Callback (Callback (..)) where
 
 import "aeson" Data.Aeson (FromJSON (parseJSON), withObject, (.:))
-import "base" Data.Int (Int)
 import "text" Data.Text (Text)
 
 import Network.Telegram.API.Bot.Object.Message (Message)
 
-data Callback = Datatext Int Message Text deriving Show
+data Callback = Datatext Text Message Text deriving Show
 
 instance FromJSON Callback where
 	parseJSON = withObject "Callback" $ \v ->
