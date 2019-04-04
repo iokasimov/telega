0c4dd1c00645767e1f354b3e7adeca8f7861c6a6
Murat Kasimov <iokasimov.m@gmail.com>
2019-04-04 14:32:06 +0300

    Define FromJSON instance for Member and add new invariant to Update datatype

--- Network/Telegram/API/Bot/Object/Member.hs
+++ Network/Telegram/API/Bot/Object/Member.hs
@@ -1,6 +1,19 @@
 module Network.Telegram.API.Bot.Object.Member (Member (..)) where
 
+import "aeson" Data.Aeson (FromJSON (parseJSON), withObject, (.:))
+import "aeson" Data.Aeson.Types (Object, Parser)
+import "base" Control.Applicative (Alternative ((<|>)))
+
 import Network.Telegram.API.Bot.Object.Chat (Chat)
 import Network.Telegram.API.Bot.Object.From (From)
 
-data Member = Left Chat From | Joined Chat [From] deriving Show
+data Member = Gone Chat From | Joined Chat [From] deriving Show
+
+instance FromJSON Member where
+	parseJSON = withObject "Member" $ \v -> gone v <|> joined v where
+
+		gone :: Object -> Parser Member
+		gone v = Gone <$> v .: "chat" <*> v .: "left_chat_member"
+
+		joined :: Object -> Parser Member
+		joined v = Joined <$> v .: "chat" <*> v .: "new_chat_members"
--- Network/Telegram/API/Bot/Object/Update.hs
+++ Network/Telegram/API/Bot/Object/Update.hs
@@ -1,32 +1,39 @@
 module Network.Telegram.API.Bot.Object.Update (Update (..), chat) where
 
 import "aeson" Data.Aeson (FromJSON (parseJSON), withObject, (.:))
-import "aeson" Data.Aeson.Types (Object, Parser)
+import "aeson" Data.Aeson.Types (Object, Parser, Value (Object))
 import "base" Control.Applicative (Alternative ((<|>)))
 import "lens" Control.Lens (Lens')
 
 import Network.Telegram.API.Bot.Object.Callback (Callback (Datatext))
 import Network.Telegram.API.Bot.Object.Chat (Chat)
+import Network.Telegram.API.Bot.Object.Member (Member (Gone, Joined))
 import Network.Telegram.API.Bot.Object.Message (Message (Textual, Command))
 import Network.Telegram.API.Bot.Property.Identifiable
 	(Identifiable (identificator), Identificator)
 
 data Update
-	= Incoming Int Message
-	| Query Int Callback
+	= Query Int Callback
+	| Membership Int Member
+	| Incoming Int Message
 	deriving Show
 
 type instance Identificator Update = Int
 
 instance Identifiable Update where
-	identificator (Incoming i _) = i
 	identificator (Query i _) = i
+	identificator (Membership i _) = i
+	identificator (Incoming i _) = i
 
 instance FromJSON Update where
-	parseJSON = withObject "Update" $ \v -> query v <|> incoming v where
+	parseJSON = withObject "Update" $ \v ->
+		query v <|> membership v <|> incoming v where
 
 		query :: Object -> Parser Update
 		query v = Query <$> v .: "update_id" <*> v .: "callback_query"
+
+		membership :: Object -> Parser Update
+		membership v = Query <$> v .: "update_id" <*> parseJSON (Object v)
 
 		incoming :: Object -> Parser Update
 		incoming v = Incoming <$> v .: "update_id" <*> v .: "message"
@@ -40,3 +47,7 @@ chat f (Query upd_id (Datatext cq_id (Command msg_id chat_ from cmd) dttxt)) =
 	(\chat' -> Query upd_id (Datatext cq_id (Command msg_id chat' from cmd) dttxt)) <$> f chat_
 chat f (Query upd_id (Datatext cq_id (Textual msg_id chat_ from txt) dttxt)) =
 	(\chat' -> Query upd_id (Datatext cq_id (Textual msg_id chat' from txt) dttxt)) <$> f chat_
+chat f (Membership upd_id (Gone chat_ users)) =
+	(\chat' -> Membership upd_id (Gone chat' users)) <$> f chat_
+chat f (Membership upd_id (Joined chat_ users)) =
+	(\chat' -> Membership upd_id (Joined chat' users)) <$> f chat_
