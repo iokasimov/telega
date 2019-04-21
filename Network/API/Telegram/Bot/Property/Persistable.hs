116d2f78ed8c33e0fc0e302b4d82d59bc0f60b68
Murat Kasimov <iokasimov.m@gmail.com>
2019-04-21 14:03:18 +0300

    Return persist_ method to Persistable type class

--- Network/API/Telegram/Bot/Property/Persistable.hs
+++ Network/API/Telegram/Bot/Property/Persistable.hs
@@ -26,9 +26,12 @@ class Persistable action where
 	endpoint :: Payload action -> String
 
 	persist :: FromJSON (Returning action) => Payload action -> Telegram e (Returning action)
-	persist x = request (endpoint x) (Object $ payload x) where
+	persist x = request (endpoint x) (Object $ payload x)
 
-		request :: forall a e . FromJSON a => String -> Value -> Telegram e a
-		request e p = snd <$> ask >>= \(session, Token token) -> lift . ExceptT . try
-			. fmap (fromJust . join . fmap result . decode @(Ok a) . responseBody)
-				. flip (post session) p $ "https://api.telegram.org/" <> unpack token <> "/" <> e
+	persist_ :: Payload action -> Telegram e ()
+	persist_ x = request @() @_ (endpoint x) (Object $ payload x)
+
+request :: forall a e . FromJSON a => String -> Value -> Telegram e a
+request e p = snd <$> ask >>= \(session, Token token) -> lift . ExceptT . try
+	. fmap (fromJust . join . fmap result . decode @(Ok a) . responseBody)
+		. flip (post session) p $ "https://api.telegram.org/" <> unpack token <> "/" <> e
