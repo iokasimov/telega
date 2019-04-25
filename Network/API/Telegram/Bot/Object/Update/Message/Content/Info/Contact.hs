bb3be3ed2d9c79a56c3952792e68f493c13776db
Murat Kasimov <iokasimov.m@gmail.com>
2019-04-25 23:37:05 +0300

    Define Accessible instances for first/last name within Contact

--- Network/API/Telegram/Bot/Object/Update/Message/Content/Info/Contact.hs
+++ Network/API/Telegram/Bot/Object/Update/Message/Content/Info/Contact.hs
@@ -4,13 +4,21 @@ import "aeson" Data.Aeson (FromJSON (parseJSON), withObject, (.:), (.:?))
 import "base" Control.Applicative ((<*>))
 import "base" Data.Function (($))
 import "base" Data.Functor ((<$>))
+import "base" Data.Int (Int)
 import "base" Data.Maybe (Maybe)
 import "base" Text.Show (Show)
 import "text" Data.Text (Text)
 
 import Network.API.Telegram.Bot.Object.Name (Name, First, Last)
+import Network.API.Telegram.Bot.Property.Accessible (Accessible (access))
 
-data Contact = Contact (First Name) (Maybe (Last Name)) (Maybe Text) Text (Maybe Text) deriving Show
+data Contact = Contact (First Name) (Maybe (Last Name)) (Maybe Int) Text (Maybe Text) deriving Show
+
+instance Accessible (First Name) Contact where
+	access f (Contact fn ln uid pn vc) = (\fn' -> Contact fn' ln uid pn vc) <$> f fn
+
+instance Accessible (Maybe (Last Name)) Contact where
+	access f (Contact fn ln uid pn vc) = (\ln' -> Contact fn ln' uid pn vc) <$> f ln
 
 instance FromJSON Contact where
 	parseJSON = withObject "Contact" $ \i -> Contact
