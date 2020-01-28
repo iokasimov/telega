42d23a18906b7453154ce467fa184cc21d68ec15
Murat Kasimov <iokasimov.m@gmail.com>
2020-01-28 10:36:10 +0300

    Replace transformers on joint

--- Network/API/Telegram/Bot/Property/Persistable.hs
+++ Network/API/Telegram/Bot/Property/Persistable.hs
@@ -1,18 +1,17 @@
 module Network.API.Telegram.Bot.Property.Persistable (Persistable (..)) where
 
 import "aeson" Data.Aeson (FromJSON, Object, Value (Object))
-import "base" Control.Exception (try)
+import "base" Control.Exception (SomeException, try)
 import "base" Control.Monad ((>>=))
 import "base" Data.Function ((.), ($))
 import "base" Data.Functor ((<$>))
 import "base" Data.Maybe (fromJust)
 import "base" Data.Monoid (mempty)
 import "base" Data.Tuple (snd)
+import "base" System.IO (IO)
 import "data-default" Data.Default (def)
+import "joint" Control.Joint (Reader, get, lift)
 import "text" Data.Text (Text)
-import "transformers" Control.Monad.Trans.Class (lift)
-import "transformers" Control.Monad.Trans.Except (ExceptT (ExceptT))
-import "transformers" Control.Monad.Trans.Reader (ask)
 import "req" Network.HTTP.Req (POST (POST), ReqBodyJson (ReqBodyJson)
 	, https, jsonResponse, req, responseBody, runReq, (/:))
 
@@ -32,7 +31,9 @@ class Persistable action where
 	persist_ x = request @() @_ (endpoint x) (Object $ payload x)
 
 request :: forall a e . FromJSON a => Text -> Value -> Telegram e a
-request e p = snd <$> ask >>= \(Token token) -> lift . ExceptT . try .
-	(<$>) (fromJust . result . responseBody) . runReq def $
-		req POST (https "api.telegram.org" /: token /: e)
+request e p = snd <$> lift @(Reader (e, Token)) get >>= lift . try @SomeException . send >>= lift where
+
+	send :: FromJSON a => Token -> IO a
+	send (Token token) = (<$>) (fromJust . result . responseBody) . runReq def
+		$ req POST (https "api.telegram.org" /: token /: e)
 			(ReqBodyJson p) (jsonResponse @(Ok a)) mempty
