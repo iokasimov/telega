54a7155895e7c4df427acceffeda068cf6773095
Murat Kasimov <iokasimov.m@gmail.com>
2025-03-23 12:30:20 +0700

    Include Utils module in library exports

--- Network/API/Telegram/Bot.hs
+++ Network/API/Telegram/Bot.hs
@@ -3,3 +3,4 @@ module Network.API.Telegram.Bot (module Exports) where
 import Network.API.Telegram.Bot.Property as Exports
 import Network.API.Telegram.Bot.Object as Exports
 import Network.API.Telegram.Bot.Core as Exports
+import Network.API.Telegram.Bot.Utils as Exports
